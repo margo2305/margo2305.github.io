fffbb3b75c6e9de6cb8dfd994d98dad77908e0d8
margo2305 <145350858+margo2305@users.noreply.github.com>
2026-05-13 22:44:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const themeToggle = document.getElementById('theme-toggle');
+    const themeBtn = document.getElementById('theme-toggle');
     const themeText = document.getElementById('theme-text');
     const body = document.body;
 
-    const savedTheme = localStorage.getItem('theme');
-    
-    if (savedTheme === 'dark') {
-        body.classList.add('dark-mode');
-        themeText.innerText = 'Light Mode';
-    }
-
-    themeToggle.addEventListener('click', () => {
+    themeBtn.addEventListener('click', () => {
         body.classList.toggle('dark-mode');
         
         if (body.classList.contains('dark-mode')) {
             themeText.innerText = 'Light Mode';
-            localStorage.setItem('theme', 'dark');
         } else {
             themeText.innerText = 'Dark Mode';
-            localStorage.setItem('theme', 'light');
         }
     });
 });
